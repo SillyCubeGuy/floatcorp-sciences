a7711778aa1ea5cb778f9eb17e575dc8cf4e6918
SillyCubeGuy <creepergaming624@gmail.com>
2024-08-23 20:46:47 -0400

    fixer upper

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
-// definitions
+// actual code
 var songtoggle = false
 window.onmousemove = function() {
     music = document.getElementById("bgm");
+}
+
+window.onblur = function() {
+    music = document.getElementById("bgm");
+    music.volume = 0.2
+}
+
+window.onfocus = function() {
+    music = document.getElementById("bgm");
+    music.volume = 0.5
 }
 
 
 function bgmcheck() {
     var music = document.getElementById("bgm");
     var mbutton = document.getElementById("mbutton");
+    music.volume = 0.5
     console.log(":>")
     songtoggle = !songtoggle; // Toggles the value (true -> false or false -> true)
     if (songtoggle) {
@@ -20,5 +31,3 @@ function bgmcheck() {
         mbutton.src = "NoPlay2.svg";
     }
 }
-
-// actual code
